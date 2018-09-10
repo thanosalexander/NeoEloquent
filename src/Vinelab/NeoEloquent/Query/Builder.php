42291e12d545f1f73575ea1519b12ddfb0470ead
Athanasios Alexandris <thanosalexander@gmail.com>
2018-09-10 17:03:27 +0300

    fixes

--- src/Vinelab/NeoEloquent/Query/Builder.php
+++ src/Vinelab/NeoEloquent/Query/Builder.php
@@ -18,14 +18,14 @@ class Builder extends IlluminateQueryBuilder {
      *
      * @var Vinelab\NeoEloquent\Connection
      */
-    protected $connection;
+    public $connection;
 
     /**
      * The database active client handler
      *
      * @var Everyman\Neo4j\Client
      */
-    protected $client;
+    public $client;
 
     /**
      * The matches constraints for the query.
@@ -46,7 +46,7 @@ class Builder extends IlluminateQueryBuilder {
      *
      * @var array
      */
-    protected $bindings = array(
+    public $bindings = array(
         'matches'=> [],
         'select' => [],
         'join'   => [],
@@ -56,11 +56,11 @@ class Builder extends IlluminateQueryBuilder {
     );
 
     /**
-	 * All of the available clause operators.
-	 *
-	 * @var array
-	 */
-    protected $operators = array(
+     * All of the available clause operators.
+     *
+     * @var array
+     */
+    public $operators = array(
         '+', '-', '*', '/', '%', '^',    // Mathematical
         '=', '<>', '<', '>', '<=', '>=', // Comparison
         'is null', 'is not null',
@@ -89,11 +89,11 @@ class Builder extends IlluminateQueryBuilder {
     }
 
     /**
-	 * Set the node's label which the query is targeting.
-	 *
-	 * @param  string  $label
-	 * @return \Vinelab\NeoEloquent\Query\Builder|static
-	 */
+     * Set the node's label which the query is targeting.
+     *
+     * @param  string  $label
+     * @return \Vinelab\NeoEloquent\Query\Builder|static
+     */
     public function from($label)
     {
         $this->from = $label;
@@ -102,12 +102,12 @@ class Builder extends IlluminateQueryBuilder {
     }
 
     /**
-	 * Insert a new record and get the value of the primary key.
-	 *
-	 * @param  array   $values
-	 * @param  string  $sequence
-	 * @return int
-	 */
+     * Insert a new record and get the value of the primary key.
+     *
+     * @param  array   $values
+     * @param  string  $sequence
+     * @return int
+     */
     public function insertGetId(array $values, $sequence = null)
     {
         // create a neo4j Node
@@ -200,10 +200,10 @@ class Builder extends IlluminateQueryBuilder {
     }
 
     /**
-    * Get the count of the total records for the paginator.
-    *
-    * @param  array  $columns
-    * @return int
+     * Get the count of the total records for the paginator.
+     *
+     * @param  array  $columns
+     * @return int
      */
     public function getCountForPagination($columns = ['*'])
     {
@@ -223,27 +223,27 @@ class Builder extends IlluminateQueryBuilder {
 
         $row = null;
         if ($results->offsetExists(0)) {
-                $row = $results->offsetGet(0);
-                $count = $row->offsetGet(0);
-                return $count;
+            $row = $results->offsetGet(0);
+            $count = $row->offsetGet(0);
+            return $count;
         } else {
-                return 0;
+            return 0;
         }
     }
 
     /**
-	 * Add a basic where clause to the query.
-	 *
-	 * @param  string  $column
-	 * @param  string  $operator
-	 * @param  mixed   $value
-	 * @param  string  $boolean
-	 * @return \Illuminate\Database\Query\Builder|static
-	 *
-	 * @throws \InvalidArgumentException
-	 */
-	public function where($column, $operator = null, $value = null, $boolean = 'and')
-	{
+     * Add a basic where clause to the query.
+     *
+     * @param  string  $column
+     * @param  string  $operator
+     * @param  mixed   $value
+     * @param  string  $boolean
+     * @return \Illuminate\Database\Query\Builder|static
+     *
+     * @throws \InvalidArgumentException
+     */
+    public function where($column, $operator = null, $value = null, $boolean = 'and')
+    {
         // First we check whether the operator is 'IN' so that we call whereIn() on it
         // as a helping hand and centralization strategy, whereIn knows what to do with the IN operator.
         if (mb_strtolower($operator) == 'in')
@@ -252,64 +252,64 @@ class Builder extends IlluminateQueryBuilder {
         }
 
         // If the column is an array, we will assume it is an array of key-value pairs
-		// and can add them each as a where clause. We will maintain the boolean we
-		// received when the method was called and pass it into the nested where.
-		if (is_array($column))
-		{
-			return $this->whereNested(function(IlluminateQueryBuilder $query) use ($column)
-			{
-				foreach ($column as $key => $value)
-				{
-					$query->where($key, '=', $value);
-				}
-			}, $boolean);
-		}
+        // and can add them each as a where clause. We will maintain the boolean we
+        // received when the method was called and pass it into the nested where.
+        if (is_array($column))
+        {
+            return $this->whereNested(function(IlluminateQueryBuilder $query) use ($column)
+            {
+                foreach ($column as $key => $value)
+                {
+                    $query->where($key, '=', $value);
+                }
+            }, $boolean);
+        }
 
-		if (func_num_args() == 2)
-		{
-			list($value, $operator) = array($operator, '=');
-		}
-		elseif ($this->invalidOperatorAndValue($operator, $value))
-		{
-			throw new \InvalidArgumentException("Value must be provided.");
-		}
+        if (func_num_args() == 2)
+        {
+            list($value, $operator) = array($operator, '=');
+        }
+        elseif ($this->invalidOperatorAndValue($operator, $value))
+        {
+            throw new \InvalidArgumentException("Value must be provided.");
+        }
 
-		// If the columns is actually a Closure instance, we will assume the developer
-		// wants to begin a nested where statement which is wrapped in parenthesis.
-		// We'll add that Closure to the query then return back out immediately.
-		if ($column instanceof Closure)
-		{
-			return $this->whereNested($column, $boolean);
-		}
+        // If the columns is actually a Closure instance, we will assume the developer
+        // wants to begin a nested where statement which is wrapped in parenthesis.
+        // We'll add that Closure to the query then return back out immediately.
+        if ($column instanceof Closure)
+        {
+            return $this->whereNested($column, $boolean);
+        }
 
-		// If the given operator is not found in the list of valid operators we will
-		// assume that the developer is just short-cutting the '=' operators and
-		// we will set the operators to '=' and set the values appropriately.
-		if ( ! in_array(mb_strtolower($operator), $this->operators, true))
-		{
-			list($value, $operator) = array($operator, '=');
-		}
+        // If the given operator is not found in the list of valid operators we will
+        // assume that the developer is just short-cutting the '=' operators and
+        // we will set the operators to '=' and set the values appropriately.
+        if ( ! in_array(mb_strtolower($operator), $this->operators, true))
+        {
+            list($value, $operator) = array($operator, '=');
+        }
 
-		// If the value is a Closure, it means the developer is performing an entire
-		// sub-select within the query and we will need to compile the sub-select
-		// within the where clause to get the appropriate query record results.
-		if ($value instanceof Closure)
-		{
-			return $this->whereSub($column, $operator, $value, $boolean);
-		}
+        // If the value is a Closure, it means the developer is performing an entire
+        // sub-select within the query and we will need to compile the sub-select
+        // within the where clause to get the appropriate query record results.
+        if ($value instanceof Closure)
+        {
+            return $this->whereSub($column, $operator, $value, $boolean);
+        }
 
-		// If the value is "null", we will just assume the developer wants to add a
-		// where null clause to the query. So, we will allow a short-cut here to
-		// that method for convenience so the developer doesn't have to check.
-		if (is_null($value))
-		{
-			return $this->whereNull($column, $boolean, $operator != '=');
-		}
+        // If the value is "null", we will just assume the developer wants to add a
+        // where null clause to the query. So, we will allow a short-cut here to
+        // that method for convenience so the developer doesn't have to check.
+        if (is_null($value))
+        {
+            return $this->whereNull($column, $boolean, $operator != '=');
+        }
 
-		// Now that we are working with just a simple query we can put the elements
-		// in our array and add the query binding to our array of bindings that
-		// will be bound to each SQL statements when it is finally executed.
-		$type = 'Basic';
+        // Now that we are working with just a simple query we can put the elements
+        // in our array and add the query binding to our array of bindings that
+        // will be bound to each SQL statements when it is finally executed.
+        $type = 'Basic';
 
         $property = $column;
 
@@ -339,11 +339,11 @@ class Builder extends IlluminateQueryBuilder {
 
         if ( ! $value instanceof Expression)
         {
-			$this->addBinding([$property => $value], 'where');
-		}
+            $this->addBinding([$property => $value], 'where');
+        }
 
-		return $this;
-	}
+        return $this;
+    }
 
     /**
      * Increment the value of an existing column on a where clause.
@@ -549,33 +549,33 @@ class Builder extends IlluminateQueryBuilder {
     }
 
     /**
-	 * Execute the query as a fresh "select" statement.
-	 *
-	 * @param  array  $columns
-	 * @return array|static[]
-	 */
-	public function getFresh($columns = array('*'))
-	{
-		if (is_null($this->columns)) $this->columns = $columns;
+     * Execute the query as a fresh "select" statement.
+     *
+     * @param  array  $columns
+     * @return array|static[]
+     */
+    public function getFresh($columns = array('*'))
+    {
+        if (is_null($this->columns)) $this->columns = $columns;
 
         return $this->runSelect();
-	}
-
-	/**
-	 * Run the query as a "select" statement against the connection.
-	 *
-	 * @return array
-	 */
-	protected function runSelect()
-	{
-		return $this->connection->select($this->toCypher(), $this->getBindings());
-	}
+    }
 
     /**
-	 * Get the Cypher representation of the traversal.
-	 *
-	 * @return string
-	 */
+     * Run the query as a "select" statement against the connection.
+     *
+     * @return array
+     */
+    protected function runSelect()
+    {
+        return $this->connection->select($this->toCypher(), $this->getBindings());
+    }
+
+    /**
+     * Get the Cypher representation of the traversal.
+     *
+     * @return string
+     */
     public function toCypher()
     {
         return $this->grammar->compileSelect($this);
@@ -850,15 +850,15 @@ class Builder extends IlluminateQueryBuilder {
         return $this->grammar->getIdReplacement($property);
     }
 
-	/**
-	 * Get a new instance of the query builder.
-	 *
-	 * @return \Illuminate\Database\Query\Builder
-	 */
-	public function newQuery()
-	{
-		return new Builder($this->connection, $this->grammar, $this->getProcessor());
-	}
+    /**
+     * Get a new instance of the query builder.
+     *
+     * @return \Illuminate\Database\Query\Builder
+     */
+    public function newQuery()
+    {
+        return new Builder($this->connection, $this->grammar, $this->getProcessor());
+    }
 
     /**
      * Fromat the value into its string representation.
